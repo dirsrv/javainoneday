62d4d00272f0ed93abc18fc485f000ff52f6494c
dirsrv <31632951+dirsrv@users.noreply.github.com>
2018-05-02 13:26:27 +0200

    1st Java Program
    1st comment

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// simple HelloWorld program
+
+public class HelloWorld
+	{
+		public static void main (String[] agrs)
+			{
+				System.out.print("this is just a test - simple Java HelloWorld program");
+				
+			}
+	}
+	
