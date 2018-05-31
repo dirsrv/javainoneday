0603e348bc01c6d6e688ce212a1170b2585cadc2
dirsrv <31632951+dirsrv@users.noreply.github.com>
2018-05-31 20:27:53 +0200

    dirsrv
    snipets with different variables

---
+++ TestClassVariables.java
@@ -0,0 +1,29 @@
+/* 
+Examples from "Learn Java in one day and LEARN IT WELL" book 
+Simple program with many variable types, chapter 3
+*/
+//package helloworld;
+
+public class TestClassVariables
+	{
+		public static void main (String[] agrs)
+			{
+				byte userAge = 20;
+				short numberOfStudent = 45;
+				int numberOfEmployees = 500;
+				long numberOfInhabitants = 21021313012678L;
+				
+				float hourlyRate = 60.5F;
+				double numberOfHours = 5120.5;
+				
+				char grade = 'A';
+				boolean promote = true;
+				
+				byte level = 2, userExperience = 5;
+				
+				System.out.print("this is just a test - simple Java HelloWorld program");
+				
+				
+			}
+	}
+	
